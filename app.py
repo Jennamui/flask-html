3b5f38a1af1e2d224a97c9959c293eac008c5224
Jenna Mui <Jenna.mui@stonybrook.edu>
2022-09-13 01:53:26 -0400

    First upload
    first update

---
+++ app.py
@@ -0,0 +1,19 @@
+from pydoc import pager
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template('p1.html')
+
+@app.route('/patients')
+def patients():
+    return render_template('p2.html')
+
+@app.route('/medications')
+def medications():
+    return render_template('p3.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=80)
